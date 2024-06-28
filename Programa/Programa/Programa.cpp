d36d396dfea7fb8a8e2b2b12f6f93b503e577257
Gromovia <164464132+Gromovia@users.noreply.github.com>
2024-06-29 00:50:28 +0300

    sozdanie obolochki programi

---
+++ Programa/Programa/Programa.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+using namespace std;
+double enterA() {
+
+}
+double stre() {
+
+}
+
+double pertre() {
+
+}
+
+int main()
+{
+    int a = 0;
+    cout << "1 - ввести положительное число А\n" << "2 - вычислить площадь равностороннего треугольника S = а2\n" << "3 - вычислить периметр равностороннего треугольника Р = 3*a* Sqrt(3) / 4\n";
+    cout << "Введите число от 1 до 3 включительно";
+    cin >> a;
+    switch (a) {
+
+    case 1: enterA;
+    case 2: stre;
+    case 3: pertre;
+     
+    default: cout << "неправильный ввод";
+    }
+
